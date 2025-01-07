b0269d0c6425a8100b2d28016d0d5b885ef2d10f
김영은 <128370837+zeroeuni@users.noreply.github.com>
2025-01-07 14:20:43 +0900

    Add : color style 추가

---
+++ src/style/thee.ts
@@ -0,0 +1,19 @@
+import { primaryKey } from 'drizzle-orm/gel-core';
+
+const theme = {
+	white: '#FFFFFF',
+	primary: '#0094FF',
+
+	gray100: '#F2F2F2',
+	gray200: '#D4D4D4',
+	gray300: '#B9B9BC',
+	gray400: '#96979C',
+	gray500: '#7A7D85',
+	gray600: '#54575F',
+	gray700: '#474A52',
+	gray800: '#343842',
+	gray900: '#282B34',
+	gray1000: '#000000',
+};
+
+export default theme;
