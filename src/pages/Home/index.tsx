18e52cdbd91dd63337501fc7bbffcafd7f6c24a8
윤서 <Yunse070708@gmail.com>
2025-01-11 19:27:10 +0900

    Fix : 버튼 정렬

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,67 +1,71 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import * as S from "./style";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import * as S from './style';
 
 const Home = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const [seButton, setSeButton] = useState<string>("고등");
+	const [seButton, setSeButton] = useState<string>('기초');
 
-  const ButtonClick = (buttonName: string) => {
-    setSeButton(buttonName);
-  };
+	const ButtonClick = (buttonName: string) => {
+		setSeButton(buttonName);
+	};
 
-  const buttons = ["고등", "중등", "기초"];
-  const boxes = [
-    {
-      title: "중학교 2학년 필수 영어 단어",
-      tag: "30 단어",
-      date: "2024.06.01",
-    },
-    { title: "수능 특강 영단어 1800", tag: "1800단어", date: "2024.03.01" },
-    {
-      title: "고등학교 1학년 필수 영어단어",
-      tag: "1800단어",
-      date: "2024.03.01",
-    },
-  ];
+	const buttons = ['기초', '중등', '고등'];
+	const boxes = [
+		{
+			title: '중학교 2학년 필수 영어 단어',
+			tag: '100 단어',
+			date: '2025.01.03',
+		},
+		{
+			title: '고등학교 1학년 필수 영어 단어',
+			tag: '1800단어',
+			date: '2025.01.10',
+		},
 
-  return (
-    <S.HomeContainer>
-      <S.HomeTitle>추천 단어 세트 모음 🎢</S.HomeTitle>
-      <S.HomeButtonContainer>
-        {buttons.map((name) => (
-          <S.HomeButton
-            key={name}
-            selected={seButton === name}
-            onClick={() => ButtonClick(name)}
-          >
-            {name}
-          </S.HomeButton>
-        ))}
-      </S.HomeButtonContainer>
-      <S.BoxContainer>
-        {boxes.map((box, index) => (
-          <S.Box onClick={() => navigate("/Click")} key={index}>
-            <S.BoxTitle>{box.title}</S.BoxTitle>
-            <S.BoxTag>{box.tag}</S.BoxTag>
-            <S.BoxDate>{box.date}</S.BoxDate>
-          </S.Box>
-        ))}
-      </S.BoxContainer>
-      <S.HomeTitle>최근 추가된 단어 세트 모음 ⚡️</S.HomeTitle>
-      <S.BoxContainer>
-        {boxes.map((box, index) => (
-          <S.Box onClick={() => navigate("/Click")} key={index}>
-            <S.BoxTitle>{box.title}</S.BoxTitle>
-            <S.BoxTag>{box.tag}</S.BoxTag>
-            <S.BoxDate>{box.date}</S.BoxDate>
-          </S.Box>
-        ))}
-      </S.BoxContainer>
-      <S.StyledLink to="/Library">라이브러리 바로가기</S.StyledLink>
-    </S.HomeContainer>
-  );
+		{
+			title: '고등학교 2학년 필수 영어 단어',
+			tag: '2000단어',
+			date: '2024.03.01',
+		},
+	];
+
+	return (
+		<S.HomeContainer>
+			<S.HomeTitle>추천 단어 세트 모음 🎢</S.HomeTitle>
+			<S.HomeButtonContainer>
+				{buttons.map((name) => (
+					<S.HomeButton
+						key={name}
+						selected={seButton === name}
+						onClick={() => ButtonClick(name)}
+					>
+						{name}
+					</S.HomeButton>
+				))}
+			</S.HomeButtonContainer>
+			<S.BoxContainer>
+				{boxes.map((box, index) => (
+					<S.Box onClick={() => navigate('/Click')} key={index}>
+						<S.BoxTitle>{box.title}</S.BoxTitle>
+						<S.BoxTag>{box.tag}</S.BoxTag>
+						<S.BoxDate>{box.date}</S.BoxDate>
+					</S.Box>
+				))}
+			</S.BoxContainer>
+			<S.HomeTitle>최근 추가된 단어 세트 모음 ⚡️</S.HomeTitle>
+			<S.BoxContainer>
+				{boxes.map((box, index) => (
+					<S.Box onClick={() => navigate('/Click')} key={index}>
+						<S.BoxTitle>{box.title}</S.BoxTitle>
+						<S.BoxTag>{box.tag}</S.BoxTag>
+						<S.BoxDate>{box.date}</S.BoxDate>
+					</S.Box>
+				))}
+			</S.BoxContainer>
+		</S.HomeContainer>
+	);
 };
 
 export default Home;
